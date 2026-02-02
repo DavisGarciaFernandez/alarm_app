0aae505c3140a570aa571f56f5871e0f52141e38
Davis Garcia Fernandez <127911758+DavisGarciaFernandez@users.noreply.github.com>
2026-02-02 15:43:55 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-
-DATA_RAW_PATH = r"C:\Users\HP\Desktop\alarms_app\data\raw\HistoricalAlarms202501-202512.xlsx"
-DATA_PROCESADA_PATH = r"C:\Users\HP\Desktop\alarms_app\data\procesada\alarmas_con_limpieza.parquet"
+BASE_DIR = Path(__file__).resolve().parent  # carpeta donde está config.py
+DATA_RAW_PATH = BASE_DIR / "data" / "raw" / "HistoricalAlarms202501-202512.xlsx"
+DATA_PROCESADA_PATH = BASE_DIR / "data" / "procesada" / "alarmas_con_limpieza.parquet"
